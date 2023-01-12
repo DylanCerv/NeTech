49a7516fb720e63b7547b5700e8f66de2d08b77e
DYlan <gownerbeats@gmail.com>
2023-01-12 14:53:11 -0500

    mm

--- pages/components/CardPrincipal/CardPrincipal.jsx
+++ pages/components/CardPrincipal/CardPrincipal.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 
 export default function CardPrincipal({img, title, description, link}) {
   const noImagen = 'https://i.pinimg.com/736x/3a/ab/e0/3aabe0e9a520b9ad90407a82f85adb42.jpg'
@@ -9,7 +8,7 @@ export default function CardPrincipal({img, title, description, link}) {
   return (
     <div className='flex flex-col gap-1'>
         <a className='mb-2 text-center' href={link} target="_blank" rel="noopener noreferrer">
-            <Image className='rounded w-full' src={img ? img : noImagen} alt="" />
+            <img className='rounded w-full' src={img ? img : noImagen} alt="" />
         </a>
         <a href={link} target="_blank" rel="noopener noreferrer">
             <h1 className='text-4xl text-[#303335] font-bold hover:text-[#4298c0]'>{title}</h1>
